16c4717f1e48b980c50387c7aa7b44295c76c807
Ryzon04 <mmardliannurofiq00@gmail.com>
2024-10-19 23:29:50 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript untuk interaksi hamburger menu
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+  });
+});
