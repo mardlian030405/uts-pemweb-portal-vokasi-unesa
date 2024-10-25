5954804554c36634ded540550b6d1c7f593b3a90
Ryzon04 <mmardliannurofiq00@gmail.com>
2024-10-25 13:46:04 +0700

    Pesan commit Anda

--- script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript untuk interaksi hamburger menu
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
-  const navLinks = document.querySelector(".nav-links");
-
-  hamburger.addEventListener("click", function () {
-    navLinks.classList.toggle("active");
-  });
-});
